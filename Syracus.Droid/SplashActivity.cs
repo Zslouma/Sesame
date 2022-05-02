cf7f2d2e0abd9828e3d81c35f68ae38384c62a71
Alan PARDOEN <a.pardoen@archimed.fr>
2022-05-02 12:26:02 +0200

    [SplashScreen] Remove black screen

--- Syracus.Droid/SplashActivity.cs
+++ Syracus.Droid/SplashActivity.cs
@@ -3,37 +3,32 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Util;
 
 namespace Syracuse.Mobitheque.Droid
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, Icon = "@drawable/logo", NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
 
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
             base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
         }
 
         // Launches the startup task
         protected override void OnResume()
         {
             base.OnResume();
-            Task startupWork = new Task(() => { SimulateStartup(); });
-
-            startupWork.Start();
+            StartActivity(typeof(MainActivity));
         }
 
         // Prevent the back button from canceling the startup process
         public override void OnBackPressed()
         {
             // Method intentionally left empty.
-        }
-
-        // Simulates background work that happens behind the splash screen
-        void SimulateStartup()
-        {
-            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
 }
